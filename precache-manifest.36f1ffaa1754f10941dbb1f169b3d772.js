2346df277f98abf411d83075cc5fd5c96f760773
RoxyRomanyshyn <iamroksa@icloud.com>
2020-09-13 20:22:00 +0300

    Updates

---
+++ precache-manifest.36f1ffaa1754f10941dbb1f169b3d772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec0dffa4f48321636f3519cdb4ccaf80",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "e0adfa9adb0f0b4ba9d5",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.e1f9cf62.chunk.css"
+  },
+  {
+    "revision": "18023903398a1eb439f0",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.99fff857.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.99fff857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0adfa9adb0f0b4ba9d5",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.23052081.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
